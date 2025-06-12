bf95697515663526ccc84bfc8ce90eada7275327
Kaynan Felipe Rodrigues Vieira <kaynanrodrigues.nt@gmail.com>
2025-06-11 23:10:29 -0300

    Update ISpecialMechanicHandler.cs

--- Inventory/Items/Armor/ISpecialMechanicHandler.cs
+++ Inventory/Items/Armor/ISpecialMechanicHandler.cs
@@ -1,10 +1,27 @@
-﻿// Interface for components that can handle special mechanics
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
+// Interface for components that can handle special mechanics
 public interface ISpecialMechanicHandler
 {
+    string MechanicId { get; }
     bool CanHandleMechanic(string mechanicId);
     void ApplyMechanic(SpecialMechanic mechanic, bool enable);
     void UpdateMechanic(SpecialMechanic mechanic, Dictionary<string, float> parameters);
     List<string> GetSupportedMechanics();
+}
+
+// Interface for effect applicators
+public interface IEffectApplicator
+{
+    EquippableEffectType EffectType { get; }
+    void ApplyEffect(float amount, PlayerStatusController statusController);
+    void RemoveEffect(float amount, PlayerStatusController statusController);
+}
+
+// Interface for trait effect handlers
+public interface ITraitEffectHandler
+{
+    string TargetStat { get; }
+    void ApplyEffect(TraitEffect effect, PlayerStatusController statusController);
+    void RemoveEffect(TraitEffect effect, PlayerStatusController statusController);
 }
