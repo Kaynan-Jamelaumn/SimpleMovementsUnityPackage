a879b376eb4ada5d0df0690cb84c57dbbc836df7
Kaynan Felipe Rodrigues Vieira <kaynanrodrigues.nt@gmail.com>
2025-06-19 11:18:32 -0300

    Feature: Implemented the effects wrappers

---
+++ Essentials/Status/StatusEffects/ConsumableEffectWrapper.cs
@@ -0,0 +1,19 @@
+﻿// ConsumableEffect wrapper
+public class ConsumableEffectWrapper : IStatusEffect
+{
+    public ConsumableEffect ConsumableEffect { get; private set; }
+
+    public ConsumableEffectWrapper(ConsumableEffect effect)
+    {
+        ConsumableEffect = effect;
+    }
+
+    public string GetEffectName() => ConsumableEffect.effectName;
+    public float GetAmount() => ConsumableEffect.GetRandomizedAmount();
+    public float GetDuration() => ConsumableEffect.GetRandomizedDuration();
+    public float GetTickCooldown() => ConsumableEffect.GetRandomizedTickCooldown();
+    public bool IsProcedural() => ConsumableEffect.isProcedural;
+    public bool IsStackable() => ConsumableEffect.isStackable;
+    public bool ShouldApply(int playerLevel = 1) => ConsumableEffect.ShouldApplyEffect(playerLevel);
+    public string GetEffectDescription() => $"{GetAmount()} {ConsumableEffect.effectType}";
+}
