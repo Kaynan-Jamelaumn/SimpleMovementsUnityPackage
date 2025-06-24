fda36b5d8b866cc4ecce71349424c7f86c49541f
Kaynan Felipe Rodrigues Vieira <kaynanrodrigues.nt@gmail.com>
2025-06-24 15:43:46 -0300

    Feature: IStatusModifier

---
+++ Essentials/Status/StatusEffects/IStatusModifier.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public interface IStatusModifier
+{
+    string ModifierId { get; }
+    string ModifierName { get; }
+    bool IsActive { get; }
+    List<StatusModification> GetStatusModifications();
+    void OnStatusModificationApplied(StatusModification modification);
+    void OnStatusModificationRemoved(StatusModification modification);
+}
---
+++ Essentials/Status/StatusEffects/StatusEffectSource.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+[Serializable]
+public class StatusModification
+{
+    public enum ModificationType
+    {
+        Flat,
+        Percentage,
+        Override
+    }
+
+    public string statName;
+    public float value;
+    public ModificationType type;
+    public bool isPermanent;
+    public float duration;
+    public string sourceId;
+
+    public StatusModification(string statName, float value, ModificationType type, bool isPermanent = true, float duration = 0f, string sourceId = "")
+    {
+        this.statName = statName;
+        this.value = value;
+        this.type = type;
+        this.isPermanent = isPermanent;
+        this.duration = duration;
+        this.sourceId = sourceId;
+    }
+}
